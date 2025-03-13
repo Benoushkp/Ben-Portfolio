8a38b7bed875b5cf9122f868ff6918e7956e7a8c
Benoush Valentine F <benoushvalentine@gmail.com>
2025-03-13 10:07:55 +0530

    Your commit message

--- ben-portfolio/vite.config.js
+++ ben-portfolio/vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/", // Ensures correct asset paths
   plugins: [react()],
   build: {
-    // Adjust the chunk size warning limit (e.g., to 600 kB)
     chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
-        // Manual chunking: group node_modules packages into separate chunks
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0];
